e4ac3f85320daeb3f23cb920f7b7bd1c0cfca75b
alirezaAJ <javanmardialireza732@gmail.com>
2022-08-10 17:02:32 +0200

    moved the quantile loss into network.py
    updated the playground to include simulations with different alpha and calib_size

--- src/model/network.py
+++ src/model/network.py
@@ -1,31 +1,10 @@
 from keras.layers import Flatten, Dense, Conv2D, Input, Dropout, Conv1D
 from keras import Model, models, layers
 from keras.initializers import GlorotNormal
+from tensorflow.keras import backend as K
+import tensorflow as tf
+
 def create_model(window_size, feature_dim, kernel_size, filter_num, dropout_rate):
-
-    input_layer = Input(shape=(window_size, feature_dim, 1), )
-    #conv layer 1
-    out = Conv2D(filters=filter_num, kernel_size=kernel_size, padding='same', activation='tanh')(input_layer)
-    #conv layer 2
-    out = Conv2D(filters=filter_num, kernel_size=kernel_size, padding='same', activation='tanh')(out)
-    #conv layer 3
-    out = Conv2D(filters=filter_num, kernel_size=kernel_size, padding='same', activation='tanh')(out)
-    #conv layer 4
-    out = Conv2D(filters=filter_num, kernel_size=kernel_size, padding='same', activation='tanh')(out)
-    #conv layer 5
-    out = Conv2D(filters=1, kernel_size=(3, 1), padding='same', activation='tanh')(out)
-    #flatten layer 
-    out = Flatten()(out)
-    #dropout layer 
-    out = Dropout(dropout_rate)(out)
-    out = Dense(1, activation="tanh")(out)
-    model = Model(inputs=input_layer, outputs=out)
-
-    return model
-
-
-
-def simple_cnn(window_size, feature_dim, kernel_size, filter_num, dropout_rate):
     
     model = models.Sequential()
     model.add(layers.Conv2D(filters=filter_num, kernel_size=kernel_size, padding='same', activation='tanh', kernel_initializer=GlorotNormal(), input_shape=(window_size, feature_dim, 1)))
@@ -39,3 +18,18 @@ def simple_cnn(window_size, feature_dim, kernel_size, filter_num, dropout_rate):
     model.add(layers.Dense(1))
 
     return model
+
+
+
+def quantile_loss(q,y_true,y_pred):
+    """
+    q -- quantile level
+    y_true -- true values
+    y_pred -- predicted values
+    """
+    diff = (y_true - y_pred)
+    mask = y_true >= y_pred
+    mask_ = y_true < y_pred
+    loss = (q * K.sum(tf.boolean_mask(diff, mask), axis=-1) - (1 - q) * K.sum(tf.boolean_mask(diff, mask_), axis=-1))
+
+    return loss
